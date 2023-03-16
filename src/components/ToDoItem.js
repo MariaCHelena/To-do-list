42192192b5b36103c8ecb1c360c81652387b94d4
Maria Helena Carvalho <96885338+MariaCHelena@users.noreply.github.com>
2023-03-16 03:11:37 -0300

    fixing li positioning and adding edit components

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -1,8 +1,12 @@
 function ToDoItem({id, title, delEvent}){
     return (
         <>
-            <li key={id}>{title}</li>
-            <button onClick={() => delEvent(id)}>Delete</button>
+            <li key={id}>
+                <span>{title}</span>
+                <button onClick={() => delEvent(id)}>Delete</button>
+                <button>Edit</button>
+                <input type="text" />
+            </li>
         </>
     )
 }
