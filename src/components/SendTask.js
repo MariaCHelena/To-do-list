38425518bf7f4b70c4aba5cf3ff09aa37a672523
Maria Helena Carvalho <96885338+MariaCHelena@users.noreply.github.com>
2023-03-17 12:21:58 -0300

    Fixing sizing bug from layout

--- src/components/SendTask.js
+++ src/components/SendTask.js
@@ -4,13 +4,13 @@ let nextId = 0
 
 function SendTask({setTask, task, setList, list}){
     return (
-        <>
+        <div>
             <input type="text" value={task} onChange={e => setTask(e.target.value)}/>
             <button onClick={() => {
                 setTask('');
                 setList([...list, {id: nextId++, title: task}])
             }}>Enviar</button>
-        </>
+        </div>
     )
 }
 
